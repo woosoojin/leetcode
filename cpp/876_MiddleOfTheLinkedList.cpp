e7f7e2e9b415d0ef6a59fb008d8083820a54cdba
woosoojin <wsj1998@naver.com>
2022-03-30 23:46:24 +0900

    add code

--- cpp/876_MiddleOfTheLinkedList.cpp
+++ cpp/876_MiddleOfTheLinkedList.cpp
@@ -11,6 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        ListNode* walker = head;
+        ListNode* runner = head;
 
+        while (runner != NULL & runner->next != NULL) {
+            walker = walker->next;
+            runner = runner->next->next;
+        }
+
+        return walker;
     }
 };

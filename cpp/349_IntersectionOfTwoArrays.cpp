c5b24ffca656a5242b2828db34c3b0a82f1e1c9f
woosoojin <wsj1998@naver.com>
2022-06-18 22:43:04 +0900

    add solutions2

--- cpp/349_IntersectionOfTwoArrays.cpp
+++ cpp/349_IntersectionOfTwoArrays.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int p1 = 0;
-        int p2 = 0;
+        unordered_set<int> setContainer(nums1.begin(), nums1.end());
         vector<int> ans;
-        set<int> s;
 
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-
-        while (p1 < nums1.size() && p2 < nums2.size()) {
-            if (nums1[p1] < nums2[p2])
-                p1++;
-            else if (nums1[p1] > nums2[p2])
-                p2++;
-            else {
-                s.insert(nums1[p1]);
-                p1++;
-                p2++;
-            }
-        }
-
-        for (auto eachValue : s) {
-            ans.push_back(eachValue);
+        for (int i = 0; i < nums2.size(); i++) {
+            if (setContainer.count(nums2[i]) > 0)
+                ans.push_back(nums2[i]);
+            setContainer.erase(nums2[i]);
         }
 
         return ans;
